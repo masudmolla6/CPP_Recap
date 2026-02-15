0aad83680f44c82ad1072f4a8219c25a3d182f2d
masudmolla6 <masudmolla2937@gmail.com>
2026-02-15 23:08:45 +0600

    first commit

---
+++ Module 1/data_type.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x=10;
+    char w='w';
+    double y=2.345;
+    cout <<x<<" " << w << " " << y << endl;
+    return 0;
+}
---
+++ Module 1/input.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    char c;
+    cin>>x >>c;
+    cout << (char)x << " " << (int)c << endl;
+    return 0;
+}
