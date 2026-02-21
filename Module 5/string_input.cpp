b315a11e4a482420a8c5b99baa10dcaa3b1b9f41
masudmolla6 <masudmolla2937@gmail.com>
2026-02-21 19:03:05 +0600

    string input with space

---
+++ Module 5/string_input.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cin.ignore();
+    string s;
+    // cin >> s;
+    getline(cin,s);
+    cout << x<< endl << s << endl;
+    return 0;
+}
