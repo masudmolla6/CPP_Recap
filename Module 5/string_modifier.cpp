ffa5b76517a7fc12728f9f7862bd4b9e846aaf0c
masudmolla6 <masudmolla2937@gmail.com>
2026-02-21 16:54:36 +0600

    String Modifier Function

---
+++ Module 5/string_modifier.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1="Hello World";
+    string s2="Hi";
+    // s1+=s2;
+    // s1.append(s2);
+    // s1.push_back('A');
+    // s1+="asdfdf";
+    // s1.pop_back();
+    // s1[0]='G';
+    // s1[5]='W';
+    // s1="Ok";
+    // s1=s2;
+    // s1.assign(s2);
+    // s1.assign("Great");
+    // s1.erase(3,2);
+    // s1.replace(6,5,"Bangladesh");
+    // s1.replace(6,0,"Bangladesh");
+    s1.insert(5,"Masud");
+    cout << s1 << endl;
+
+    return 0;
+}
