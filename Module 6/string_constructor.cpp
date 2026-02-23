9024f3cbc0a05138da85a0f5c89211920b21b216
masudmolla6 <masudmolla2937@gmail.com>
2026-02-23 11:34:29 +0600

    string constructor

---
+++ Module 6/string_constructor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // string s="Hello";
+    // way-1
+    // string s("Hello World");
+    // way 2
+    // string s("Hello", 4);
+    // way 3
+    // string s="Hello World";
+    // string t(s,3);
+    // way 4
+    // string s(5, 'A');
+    cout << s << endl;
+    return 0;
+}
