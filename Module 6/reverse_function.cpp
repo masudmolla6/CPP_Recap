2dff478bb44ee2c0f1fc0ad81967ab0d762e289c
masudmolla6 <masudmolla2937@gmail.com>
2026-02-23 15:45:23 +0600

    reverse array and string

---
+++ Module 6/reverse_function.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Array Reverse
+    // int n;
+    // cin >> n;
+
+    // int a[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    // }
+
+    // reverse(a, a+n);
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+
+    // string Reverse
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+    
+    return 0;
+}
